0a8f8229fb9d7c8bc7ca6d90206c63deaf60900b
maxs1m <maxs1m@mail.ru>
2021-12-08 17:08:08 +0300

    аывф

--- src/components/PicturesList.tsx
+++ src/components/PicturesList.tsx
@@ -43,7 +43,6 @@ const PictureList:React.FC<PropsFromRedux> = (props) => {
       </View>
     </TouchableOpacity>
   );
-//ывфы
   return <>
     <FlatList
       contentContainerStyle={styles.main}
